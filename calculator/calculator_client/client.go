438165c9cb235f5e8bd84e24b0181b4c68d24ada
Yoga Hanggara <yohang88@gmail.com>
2020-09-03 01:59:27 +0700

    server streaming
    Signed-off-by: Yoga Hanggara <yohang88@gmail.com>

--- calculator/calculator_client/client.go
+++ calculator/calculator_client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/yohang88/learn-golang-grpc/calculator/calculatorpb"
 	"google.golang.org/grpc"
+	"io"
 	"log"
 )
 
@@ -19,18 +20,51 @@ func main()  {
 
 	defer conn.Close()
 
-	rpcClient := calculatorpb.NewCalculatorServiceClient(conn)
+	client := calculatorpb.NewCalculatorServiceClient(conn)
+
+	doServerStreaming(client)
+}
+
+func doUnary(client calculatorpb.CalculatorServiceClient) {
+	fmt.Println("Starting to do a unary RPC...")
 
 	request := &calculatorpb.SumRequest{
 		FirstInteger:  40,
 		SecondInteger: 20,
 	}
 
-	response, err := rpcClient.Sum(context.Background(), request)
+	response, err := client.Sum(context.Background(), request)
 
 	if err != nil {
 		log.Fatalf("Error while calling Sum RPC: %v", err)
 	}
 
 	log.Printf("Response from Sum: %v", response)
+}
+
+func doServerStreaming(client calculatorpb.CalculatorServiceClient) {
+	fmt.Println("Starting to do a server streaming RPC...")
+
+	request := &calculatorpb.PrimeNumberDecompositionRequest{
+		InputInteger: 123456789,
+	}
+
+	stream, err := client.PrimeNumberDecomposition(context.Background(), request)
+
+	if err != nil {
+		log.Fatalf("Error while calling PrimeNumberDecomposition RPC: %v", err)
+	}
+
+	for {
+		message, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Error while reading stream: %v", err)
+		}
+
+		fmt.Printf("Response from RPC: %v\n", message.GetResult())
+	}
 }
--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -11,6 +11,32 @@ import (
 
 type server struct {}
 
+func (s server) PrimeNumberDecomposition(request *calculatorpb.PrimeNumberDecompositionRequest, stream calculatorpb.CalculatorService_PrimeNumberDecompositionServer) error {
+	fmt.Printf("Prime number decomposition was invoked with %v\n", request)
+
+	inputInteger := request.GetInputInteger()
+	k := int32(2)
+
+	for inputInteger > 1 {
+		if inputInteger % k == 0 {
+			err := stream.Send(&calculatorpb.PrimeNumberDecompositionResponse{
+				Result: k,
+			})
+
+			if err != nil {
+				log.Fatalf("Error send response stream")
+			}
+
+			inputInteger = inputInteger / k
+		} else {
+			k = k + 1
+			fmt.Printf("Divisor has increased to %v\n", k)
+		}
+	}
+
+	return nil
+}
+
 func (s server) Sum(ctx context.Context, request *calculatorpb.SumRequest) (*calculatorpb.SumResponse, error) {
 	fmt.Printf("Sum function was invoked with %v", request)
 
@@ -26,8 +52,8 @@ func (s server) Sum(ctx context.Context, request *calculatorpb.SumRequest) (*cal
 	return response, nil
 }
 
-func main()  {
-	fmt.Printf("CalculatorService start to listening...")
+func main() {
+	fmt.Println("CalculatorService start to listening...")
 
 	listener, err := net.Listen("tcp", "0.0.0.0:50002")
 
